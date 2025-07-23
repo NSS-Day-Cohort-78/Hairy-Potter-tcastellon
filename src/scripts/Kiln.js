54580a527695eaa055ecd7d45e014484ac836554
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-07-23 11:26:00 -0500

    Updated Kiln and Main modules

--- src/scripts/Kiln.js
+++ src/scripts/Kiln.js
@@ -1,7 +1,20 @@
 //Define and export a function named firePottery that accepts these values as input: an object representing a piece of pottery that was made at the wheel in the makePottery function and number specifying the firing temperature of the kiln.
+export const firePottery = (madePottery, temp) => {
+    
+    //The function must add a new property of fired with the value of true to the object
+    madePottery.fired = true
+    
+    //The function must add a new property of cracked to the object: if the temperature of the kiln is above 2200 degrees then cracked property must have a value of true, if the temperature of the kiln is at or below 2200 degrees then cracked property must have a value of false.
+    if(temp > 2200) {
+        madePottery.cracked = true
+    } else if(temp <= 2200) {
+        madePottery.cracked = false
+    }
 
-//The function must add a new property of fired with the value of true to the object
+    //return both new properties after they have been added
+    return madePottery
+}
 
-//The function must add a new property of cracked to the object: if the temperature of the kiln is above 2200 degrees then cracked property must have a value of true, if the temperature of the kiln is at or below 2200 degrees then cracked property must have a value of false.
 
-//return both new properties after they have been added
+
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 // Imports go first
 import { makePottery } from './PotteryWheel.js'
-
+import { firePottery } from './Kiln.js'
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("mug", 5, 3)
@@ -9,11 +9,17 @@ let vase = makePottery("vase", 8, 7)
 let ashtray = makePottery("ashtray", 4, 2)
 let bowl = makePottery("bowl", 4, 4)
 
-console.log(mug)
-console.log(platter)
-console.log(vase)
-console.log(ashtray)
-console.log(bowl)
+let mugs = firePottery(mug, 1800)
+let platters = firePottery(platter, 2000)
+let vases = firePottery(vase, 2250)
+let ashtrays = firePottery(ashtray, 1975)
+let bowls = firePottery(bowl, 2135)
+
+console.log(mugs)
+console.log(platters)
+console.log(vases)
+console.log(ashtrays)
+console.log(bowls)
 
 // Fire each piece of pottery in the kiln
 
