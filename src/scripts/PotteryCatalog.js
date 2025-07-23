53c9a76a00223d577b9927914f550265f61aa0fe
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-07-23 15:13:32 -0500

    Updated PotteryCatalog module

--- src/scripts/PotteryCatalog.js
+++ src/scripts/PotteryCatalog.js
@@ -8,7 +8,7 @@ export const toSellOrNotToSell = (maybeForSalePottery) => {
     //If weight of pottery is greater than or equal to 6 then function must add a price property with a value 40
     //If weight of pottery is less than 6 then function must add a price property with a value 20
     if(maybeForSalePottery.cracked === true) {
-        return
+        maybeForSalePottery
     } else if(maybeForSalePottery.weight >= 6) {
         maybeForSalePottery.price = 40
     } else if(maybeForSalePottery.weight < 6) {
